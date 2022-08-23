f5b9e11f47e2afe811ea45ad91894fd661145092
Andika Bahari <kzlfxx@gmail.com>
2022-08-23 21:52:39 +0700

    feat: menambahkan fitur A

--- index.ts
+++ index.ts
@@ -1,16 +1,22 @@
 interface Human {
   name: string
   isAlive: boolean
+  age: string
+  canSwim: boolean
 }
 
 const showHumanInfo = (human: Human): void => {
   console.log(`Name: ${human.name}`)
   console.log(`Alive: ${human.isAlive ? 'yes' : 'no'}`)
+  console.log(`Age: ${human.age}`)
+  console.log(`This human ${human.canSwim ? 'can' : "can't"} swim`)
 }
 
 const human: Human = {
   name: 'Andika Bahari',
   isAlive: true,
+  age: '21',
+  canSwim: false,
 }
 
 showHumanInfo(human)
